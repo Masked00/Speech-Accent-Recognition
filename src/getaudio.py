3be2f4f746bb9876746f0c749c5f81fe5f8a5d54
SirDankenstein <52797753+SirDankenstien@users.noreply.github.com>
2021-12-02 16:19:10 +0530

    Update getaudio.py
    - removed wait
    - added colaaars :)

--- src/getaudio.py
+++ src/getaudio.py
@@ -3,11 +3,22 @@ import urllib.request
 import os
 import sys
 from pydub import AudioSegment
+from colorama import init, Fore, Style
 
+# colors
+
+init(autoreset=True)
+red = Fore.RED + Style.BRIGHT
+green = Fore.GREEN + Style.BRIGHT
+yellow = Fore.YELLOW + Style.BRIGHT
+blue = Fore.BLUE + Style.BRIGHT
+magenta = Fore.MAGENTA + Style.BRIGHT
+cyan = Fore.CYAN + Style.BRIGHT
+white = Fore.WHITE + Style.BRIGHT
 
 class GetAudio:
 
-    def __init__(self, csv_filepath, destination_folder='audio/', wait=1.5, debug=False):
+    def __init__(self, csv_filepath, destination_folder='audio/', wait=0, debug=False):
         '''
         Initializes GetAudio class object
         :param destination_folder (str): Folder where audio files will be saved
@@ -19,7 +30,7 @@ class GetAudio:
         self.url = 'http://chnm.gmu.edu/accent/soundtracks/{}.mp3'
         self.destination_folder = destination_folder
         self.wait = wait
-        self.debug = False
+        self.debug = True
 
     def check_path(self):
         '''
@@ -27,8 +38,7 @@ class GetAudio:
         '''
         if not os.path.exists(self.destination_folder):
             if self.debug:
-                print('{} does not exist, creating'.format(
-                    self.destination_folder))
+                print('{} does not exist, creating'.format(self.destination_folder))
             os.makedirs('../' + self.destination_folder)
 
     def get_audio(self):
@@ -45,12 +55,11 @@ class GetAudio:
         for lang_num in self.audio_df['language_num']:
             if not os.path.exists(self.destination_folder + '{}.wav'.format(lang_num)):
                 if self.debug:
-                    print('downloading {}'.format(lang_num))
+                    print(f'  {white}> {magenta}downloading {white}{lang_num}')
                 (filename, headers) = urllib.request.urlretrieve(
                     self.url.format(lang_num))
                 sound = AudioSegment.from_mp3(filename)
-                sound.export('../' + self.destination_folder +
-                             "{}.wav".format(lang_num), format="wav")
+                sound.export('../' + self.destination_folder + "{}.wav".format(lang_num), format="wav")
                 counter += 1
 
         return counter
